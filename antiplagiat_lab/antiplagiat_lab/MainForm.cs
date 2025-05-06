4b7ed95ca23a3a3f717ae9c0e33d0aadade420dd
puraa <puraaleksej@gmail.com>
2025-05-06 18:10:35 +0300

    naba

--- antiplagiat_lab/antiplagiat_lab/MainForm.cs
+++ antiplagiat_lab/antiplagiat_lab/MainForm.cs
@@ -11,6 +11,8 @@ namespace antiplagiat_lab
 {
   public partial class MainForm : Form
   {
+    //test
+    //234
     private const string DataFilePath = "data.json";
     private const string ReportsDirectory = "Отчёты";
     private List<Group> groups = new List<Group>();
